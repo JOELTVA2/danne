18385689fbef1ab7dd382273ed032244a3404807
JOELTVA <joeltva@hotmail.com>
2017-02-01 14:37:07 +0100

    123

--- Programkonstruktion/DAL/ConnectionString.cs
+++ Programkonstruktion/DAL/ConnectionString.cs
@@ -9,9 +9,11 @@ namespace DAL
     public class ConnectionString
     {
 
+        //byt connectionstring här
         public static string connString = "Data Source=DESKTOP-THBJUU3\\SQLEXPRESS;Initial Catalog = LabSQL2; User ID = sa; Password=hej123";
 
         public static string connStringLaptop = "Data Source=JOELTVA2\\JOELSDATABAS;Initial Catalog=LabSQL2;Integrated Security=False;User ID=sa;Password=123;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
 
     }
 
